5179f8b0c7d0ab69da5a5bf5934108ce4ef46da5
aka2003deb <153760835+aka2003deb@users.noreply.github.com>
2024-03-31 18:12:36 +0530

    Update exp6.m

--- exp6.m
+++ exp6.m
@@ -1,45 +1,37 @@
 %delta modulation
-clc
-clear all
-close all
-
+close all;
+clear all;
 a=2;
 t=0:2*pi/50:2*pi;
 x=a*sin(t);
 l=length(x);
-plot(x,'r');
-delta=0.2;
-hold on;
+plot(x,'r')
+delta=0.2
+hold on
 xn=0;
-
 for i=1:l
     if x(i)>xn(i)
-        d(i)=1;
-        xn(i+1)=xn(i)+delta;
+        d(i)=1
+        xn(i+1)=xn(i)+delta
     else
         d(i)=0;
         xn(i+1)=xn(i)-delta;
     end
 end
-
-stairs(xn);
-hold on;
-
+stairs(xn)
+hold on
 for i=1:d
     if d(i)>xn(i)
-        d(i)=0;
-        xn(i+1)=xn(i)-delta;
+        d(i)=0
+        xn(i+1)=xn(i)-delta
     else
-        d(i)=1;
-        xn(i+1)=xn(i)+delta;
+        d(i)=1
+        xn(i+1)=xn(i)+delta
     end
 end
-
-plot(xn,'c');
-title('Step Size of 0.2');
-xlabel('Time');
-ylabel('Amplitude');
-legend('Original Signal','Delta Modulated Signal','Demodulated Signal');
+plot(xn,'c')
+legend('Analog Signal','DM with step size=0.2')
+title('DELTA MODULATION')
 
 
 % adaptive delta modulation
